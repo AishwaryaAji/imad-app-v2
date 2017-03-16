3e9ae688aaca358bb3094c2dc7b735271583963d
AishwaryaAji <ajisuni64@gmail.com>
2017-03-16 22:47:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,8 @@ button.onclick = function(){
     //make a request to counter input
     //capture the response and store it in a variable
     //Rende the variable in the correr span
-    counter=counter+1;
-    var span = document.getElementById('counter');
+    counter=counter + 1;
+    var span = document.getElementById('count');
 
-    span.innerHTML = counter.toString();
+    span.innerHTML =counter.toString();
 }
